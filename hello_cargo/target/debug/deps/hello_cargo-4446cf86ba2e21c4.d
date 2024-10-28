fac9e1ac148c480c77773fad6765dbc98a2be17f
FlameFish <mccoa499@gmail.com>
2024-10-28 13:04:31 -0700

    Moved projects and practice into their own directories.

--- hello_cargo/target/debug/deps/hello_cargo-4446cf86ba2e21c4.d
+++
@@ -1,5 +0,0 @@
-/Users/ash/Documents/rust-learning/hello_cargo/target/debug/deps/libhello_cargo-4446cf86ba2e21c4.rmeta: src/main.rs
-
-/Users/ash/Documents/rust-learning/hello_cargo/target/debug/deps/hello_cargo-4446cf86ba2e21c4.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/deps/hello_cargo-d1d60bd97baee10c.d
+++
@@ -1,5 +0,0 @@
-/Users/ash/Documents/rust-learning/hello_cargo/target/debug/deps/libhello_cargo-d1d60bd97baee10c.rmeta: src/main.rs
-
-/Users/ash/Documents/rust-learning/hello_cargo/target/debug/deps/hello_cargo-d1d60bd97baee10c.d: src/main.rs
-
-src/main.rs:
